90f820f7aad9955b5d4003177c711a5fd2fe73a2
PamelaMarinelli <marinelli.1751583@studenti.uniroma1.it>
2022-05-11 17:12:46 +0200

    cambiato nome al database

--- accedi-registrati/config.php
+++ accedi-registrati/config.php
@@ -3,7 +3,7 @@
     $host = "127.0.0.1";
     $user = "root";
     $password = "";
-    $db = "login";
+    $db = "diy-recipes_db";
 
     $connessione = new mysqli($host, $user, $password, $db);
     if($connessione === false){die("Errore durante la connessione: " . $connessione->connect_error);}
